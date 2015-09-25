268e40b4c76313941a41e51dd369d084db52ce26
Stephen Kuenzli <skuenzli@weblogng.com>
2015-09-25 12:12:43 -0700

    Create the 'grakka' grails+akka application to experiment.

---
+++ grails-app/controllers/grakka/InternalServerErrorController.groovy
@@ -0,0 +1,11 @@
+package grakka
+
+class InternalServerErrorController {
+
+    def index() {
+        render(contentType: 'application/json') {
+            error = 500
+            message = "Internal server error"
+        }
+    }
+}
